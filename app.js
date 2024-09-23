dbad789805842ea7d1050eff4dc70f6d764bfc6b
Nesrinels <lounissi.nesrine@gmail.com>
2024-09-23 19:53:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/users');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json()); // For parsing JSON requests
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch((error) => console.error('MongoDB connection error:', error));
+
+// Routes
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ db/db.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://localhost:27017";
+let dbConnection;
+
+module.exports = {
+  connectToDb: (cb) => {
+    MongoClient.connect(uri)
+      .then((client) => {
+        dbConnection = client.db("learn2");
+        return cb();
+       })
+      .catch((err) => {
+       console.log(err);
+       return cb(err);
+       });
+  },
+  getDb: () => dbConnection,
+};
